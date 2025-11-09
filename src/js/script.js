757ed05c668cde351460a9c11c8d1d77897338b5
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-11-09 20:30:05 +0100

    add: APIController to api calls

--- src/js/script.js
+++ src/js/script.js
@@ -1,46 +1,57 @@
-const idClient = '';
-const secrecKey = '';
+const APIController = function () {
+  const idClient = '';
+  const secrecKey = '';
 
-const getToken = async () => {
-  const response = await fetch('https://accounts.spotify.com/api/token', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      Authorization: 'Basic ' + btoa(idClient + ':' + secrecKey),
-    },
-    body: new URLSearchParams({ grant_type: 'client_credentials' }),
-  });
+  const getToken = async () => {
+    const response = await fetch('https://accounts.spotify.com/api/token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Authorization: 'Basic ' + btoa(idClient + ':' + secrecKey),
+      },
+      body: new URLSearchParams({ grant_type: 'client_credentials' }),
+    });
 
-  const data = await response.json();
-  return data.access_token;
-};
+    const data = await response.json();
+    return data.access_token;
+  };
 
-const getGenres = async (token) => {
-  const res = await fetch(
-    'https://api.spotify.com/v1/browse/categories?locale=en_US',
-    {
+  const getGenres = async (token) => {
+    const res = await fetch(
+      'https://api.spotify.com/v1/browse/categories?locale=en_US',
+      {
+        headers: { Authorization: 'Bearer ' + token },
+      }
+    );
+
+    const data = await res.json();
+    return data.categories.items;
+  };
+
+  const getTracksList = async (token, tracksUrl) => {
+    const res = await fetch(tracksUrl + '?limit=10', {
       headers: { Authorization: 'Bearer ' + token },
-    }
-  );
+    });
 
-  const data = await res.json();
-  return data.categories.items;
+    const data = await res.json();
+    return data.items;
+  };
+
+  const getTrack = async (token, trackUrl) => {
+    const res = await fetch(trackUrl, {
+      headers: { Authorization: 'Bearer ' + token },
+    });
+
+    const data = await res.json();
+    return data;
+  };
+
+  return {
+    getToken,
+    getGenres,
+    getTracksList,
+    getTrack,
+  };
 };
 
-const getTracksList = async (token, tracksUrl) => {
-  const res = await fetch(tracksUrl + '?limit=10', {
-    headers: { Authorization: 'Bearer ' + token },
-  });
-
-  const data = await res.json();
-  return data.items;
-};
-
-const getTrack = async (token, trackUrl) => {
-  const res = await fetch(trackUrl, {
-    headers: { Authorization: 'Bearer ' + token },
-  });
-
-  const data = await res.json();
-  return data;
-};
+const api = APIController();
