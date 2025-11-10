9e65583cf7e319676cbb20cd41b0d764abedd89f
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-11-10 21:45:59 +0100

    add: helper function to get Spotify token

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 require('dotenv').config();
 const express = require('express');
 const fetch = require('node-fetch');
+const cors = require('cors');
 
 const app = express();
-app.use(corse());
+app.use(cors());
 app.use(express.json());
 
 const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
+
+async function getSpotifyToken() {
+  const authHeader = Buffer.from(`${clientId}:${clientSecret}`).toString(
+    'base64'
+  );
+
+  const response = await fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Authorization: 'Basic ' + authHeader,
+    },
+    body: new URLSearchParams({
+      grant_type: 'client_credentials',
+    }),
+  });
+
+  const data = await response.json();
+  return data.access_token;
+}
